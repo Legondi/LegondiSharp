3d2adc5e289acb3d8b13906795ca87c2177ab2c2
Legondi <leogdias12@gmail.com>
2025-05-13 16:34:32 -0300

    save

--- Code/obj/Debug/net9.0/Code.AssemblyInfo.cs
+++ Code/obj/Debug/net9.0/Code.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Code")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+16471c86cd9c4a9bea35ceaf9b42793f390e1b95")]
 [assembly: System.Reflection.AssemblyProductAttribute("Code")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Code")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
